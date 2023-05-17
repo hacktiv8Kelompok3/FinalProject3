540157757279781a0c03a73003a59405a8537c06
uilexy <dwilexy.002@gmail.com>
2023-05-17 15:07:48 +0700

    #4 menambahkan seeders

---
+++ seeders/20230517072949-seeding_category.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const categoryData = [
+  {
+    id: '1',
+    type: 'type1',
+    sold_product_amount: '1',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Categories', categoryData, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Categories', null, {})
+  }
+};
